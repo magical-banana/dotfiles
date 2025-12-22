e9a4529c8a4dc7940e520c36cf494dab38af2716
minh.d.nguyen <minh.d.nguyen@mercatus.com>
2025-12-22 13:06:15 +0700

    chore: move the PKG var outside to make it easier to add and remove later

--- scripts/install_sys_deps.sh
+++ scripts/install_sys_deps.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 echo "⚙️  Setup System Dependencies..."
 
+DEBIAN_PKGS=(zsh curl git vim stow gawk gettext make jq yq htop build-essential libssl-dev)
+FEDORA_PKGS=(zsh curl git vim stow gawk gettext make jq yq htop gcc openssl-devel)
+
 if [ -f /etc/debian_version ]; then
-    PKGS=(zsh curl git vim stow build-essential gettext gawk libssl-dev)
-    echo "ℹ️  Debian base detected, the following pkgs will be installed: $PKGS"
+    echo "ℹ️  Debian base detected..."
     sudo apt-get update
-    for pkg in "${PKGS[@]}"; do
+    for pkg in "${DEBIAN_PKGS[@]}"; do
         dpkg -s "$pkg" &>/dev/null || sudo apt-get install -y "$pkg"
     done
 elif [ -f /etc/fedora-release ]; then
-    PKGS=(zsh curl git vim stow gcc gawk make openssl-devel)
-    echo "ℹ️  Fedora base detected, the following pkgs will be installed: $PKGS"
-    for pkg in "${PKGS[@]}"; do
+    echo "ℹ️  Fedora base detected..."
+    for pkg in "${FEDORA_PKGS[@]}"; do
         rpm -q "$pkg" &>/dev/null || sudo dnf install -y "$pkg"
     done
 fi
